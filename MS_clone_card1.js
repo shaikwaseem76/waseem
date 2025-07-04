5151e7e539a302f00582485533a6510cf18394fb
shaikwaseem76 <9985791051waseem@gmail.com>
2025-07-04 11:20:09 +0530

    Add files via upload

---
+++ MS_clone_card1.js
@@ -0,0 +1,23 @@
+function card1() {
+    window.open("/My_creation/Whatapp_2nd.html", "_self");
+}
+
+function card2(){
+    window.open("/My_creation/iTunes_2nd.html", "_self");
+}
+
+function card3(){
+    window.open("/My_creation/Instagram_2nd.html", "_self");
+}
+
+function card4(){
+    window.open("/My_creation/Spotify_2nd.html", "_self");
+}
+
+function card5(){
+    window.open("/My_creation/Discord_2nd.html", "_self");
+}
+
+function card6(){
+    window.open("/My_creation/icloud_2nd.html", "_self");
+}
